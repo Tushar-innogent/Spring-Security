b95a88b945b334b9073e5bd0b26bca6c7326968b
Tushar-innogent <166622506+Tushar-innogent@users.noreply.github.com>
2024-05-16 11:26:53 +0530

    Delete exceptions directory

--- exceptions/InvalidUserInputException.java
+++
@@ -1,9 +0,0 @@
-package com.java.user.crud.project.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class InvalidUserInputException extends GenericException{
-    public InvalidUserInputException(String message, HttpStatus httpStatus){
-        super (message,httpStatus);
-    }
-}
--- exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.java.user.crud.project.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class UserNotFoundException extends GenericException{
-    public UserNotFoundException(String errorMessage) {
-        super(errorMessage, HttpStatus.BAD_REQUEST);
-    }
-}
